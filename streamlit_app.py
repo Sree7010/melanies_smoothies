f55b9687d4a5afa5b7c8897a1b2fd688738185eb
Sree7010 <jsreejith1017@gmail.com>
2024-12-31 21:30:51 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -27,14 +27,14 @@ st.stop()
 ingredients_list = st.multiselect('Choose up to 5 ingredients:', my_dataframe, max_selections=5)
 if ingredients_list:
     ingredients_string =""
-    for fruits_chosen in ingredients_list:
+    for fruit_chosen in ingredients_list:
         ingredients_string += fruits_chosen + " "
         search_on=pd_df.loc[pd_df['FRUIT_NAME'] == fruit_chosen, 'SEARCH_ON'].iloc[0]
         # st.write('The search value for ', fruit_chosen,' is ', search_on, '.')
         
-        st.subheader(fruits_chosen + 'Nutrition Information')
-        smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/"+ search_on)
-        sf_df = st.dataframe(data=smoothiefroot_response.json(), use_container_width=True)
+        st.subheader(fruit_chosen + 'Nutrition Information')
+        fruityvice_response = requests.get("https://my.fruityvice.com/api/fruit/"+ search_on)
+        fv_df = st.dataframe(data=fruityvice_response.json(), use_container_width=True)
 
 
     # Build a SQL Insert Statement & Test It
